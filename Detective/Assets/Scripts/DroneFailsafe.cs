5f31e437496113095e91ce595a7ede3d186a83a9
Jakkalantern <jakkalantern@gmail.com>
2022-07-03 19:14:56 -0700

    drone direction
    drone changes direction when the player does

--- Detective/Assets/Scripts/DroneFailsafe.cs
+++ Detective/Assets/Scripts/DroneFailsafe.cs
@@ -6,11 +6,13 @@ using static Unity.Mathematics.math;
 public class DroneFailsafe : MonoBehaviour
 {
     [SerializeField] private GameObject dest;
+    [SerializeField] private GameObject play;
 
     // Start is called before the first frame update
     void Start()
     {
         dest = GameObject.Find("PlayerOffset");
+        play = GameObject.Find("PlayerBody");
     }
 
     // Update is called once per frame
@@ -22,6 +24,15 @@ public class DroneFailsafe : MonoBehaviour
         if (Vector2.Distance(position, transform.position) > 15.0)
         {
             transform.position  = position;
+        }
+
+        if (play.transform.localScale[0] == 1f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (play.transform.localScale[0] == -1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     }
 }
